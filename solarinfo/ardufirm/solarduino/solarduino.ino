c955b9cb4279afa7c0792d08ed0bd6bfd5acae2f
hpl <logsoft@gmail.com>
2013-05-07 23:28:43 +0200

    arduino relais control for loading ready in testing

--- solarinfo/ardufirm/solarduino/solarduino.ino
+++ solarinfo/ardufirm/solarduino/solarduino.ino
@@ -7,19 +7,15 @@
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 0, 42);
-IPAddress gateway(192,168,0,1);
+IPAddress gateway(192,168,0,1);	
 IPAddress subnet(255, 255, 255, 0);
 IPAddress destination(192, 168, 0, 72);
 unsigned int localPort = 8888;      // local port to listen on
 unsigned int destPort = 8888;
-// buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char  ReplyBuffer[] = "acknowledged";       // a string to send back
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
 
-long lastReadingTime = 0;
 int relais_Pin = 9;      // select the pin for the LED
 int sol_volt_Pin = A5;      // select the pin for the LED
 int batt_volt_Pin = A4;
@@ -38,41 +34,23 @@ float sol_ratio = (float)sol_R1 / (float)sol_R2;
 float battery_Voltage = 0.0;
 float solar_Voltage = 0.0;
 
+long lastReadingTime = 0;
+long lastTestTime = 0;
+long lastTestStart = 0;
+
+boolean relais_state = false;
+boolean test_mode = true;
+
+
 void setup() {
   // start the Ethernet and UDP:
   Ethernet.begin(mac,ip);
   Udp.begin(localPort);
-
   Serial.begin(9600);
-
-  //inputoutput config
-  pinMode(relais_Pin, OUTPUT);
-
-}
-
-
-void send_udp(){
-  // send a reply, to the IP address and port that sent us the packet we received
-  Udp.beginPacket(destination, destPort);
-  Udp.write("solar volt");
-  Udp.write(";");
-  Udp.print(solar_Voltage);
-  Udp.write(";");
-  Udp.write("battery volt");
-  Udp.write(";");
-  Udp.print(battery_Voltage);
-  Udp.endPacket();
-}
-
-void relais(float volt){
-  if (volt >= 14.0){
-    digitalWrite(relais_Pin, LOW);
-    Serial.println("Relais OFF");
-  }
-  else if (volt <= 13.5){
-    digitalWrite(relais_Pin, HIGH);
-    Serial.println("Relais ON");
-  }
+  //inputoutput config 
+  pinMode(relais_Pin, OUTPUT);  
+  Serial.println(" MOOGAA SOOOLAA V0.1 ");
+  Serial.println(" says hello im alive! ");  
 }
 
 void read_analog (){
@@ -87,76 +65,118 @@ void read_analog (){
   }
 }
 
-void calc_battery_volt(){
+void calc_volt(){
   float avgval = batt_sampleVolt / 10; //divide by 10 (number of samples) to get a steady reading
   float pinVoltage = avgval * 0.00685;       //  Calculate the voltage on the A/D pin
-  /*  A reading of 1 for the A/D = 0.0048mV
-   if we multiply the A/D reading by 0.00488 then
-   we get the voltage on the pin.
-
-   NOTE! .00488 is ideal. I had to adjust
-   to .00610 to match fluke meter.
-
-   Also, depending on wiring and
-   where voltage is being read, under
-   heavy loads voltage displayed can be
-   well under voltage at supply. monitor
-   at load or supply and decide.
-   */
-
-  //float ratio = (float)R1 / (float)R2;
   battery_Voltage = pinVoltage * batt_ratio;    //  Use the ratio calculated for the voltage divider
-  //  to calculate the battery voltage
-}
 
-void calc_solar_volt(){
-  float avgval = sol_sampleVolt / 10; //divide by 10 (number of samples) to get a steady reading
-  float pinVoltage = avgval * 0.00525;       //  Calculate the voltage on the A/D pin
+  avgval = sol_sampleVolt / 10; //divide by 10 (number of samples) to get a steady reading
+  pinVoltage = avgval * 0.00525;       //  Calculate the voltage on the A/D pin
+  solar_Voltage = pinVoltage * sol_ratio;    //  Use the ratio calculated for the voltage divider
   /*  A reading of 1 for the A/D = 0.0048mV
    if we multiply the A/D reading by 0.00488 then
-   we get the voltage on the pin.
-
+   we get the voltage on the pin. 
+   
    NOTE! .00488 is ideal. I had to adjust
    to .00610 to match fluke meter.
-
+   
    Also, depending on wiring and
    where voltage is being read, under
    heavy loads voltage displayed can be
    well under voltage at supply. monitor
    at load or supply and decide.
    */
-
-  //float ratio = (float)R1 / (float)R2;
-
-  solar_Voltage = pinVoltage * sol_ratio;    //  Use the ratio calculated for the voltage divider
-  //  to calculate the battery voltage
 }
 
+void send_serial(){
 
-void loop() {
-
-  // check for a reading no more than once a second.
-  if (millis() - lastReadingTime > 1000){
-    read_analog();
-    calc_battery_volt();
-    calc_solar_volt();
-    send_udp();
-    //relais mit solar voltage aufrufen
-    relais(solar_Voltage);
-
-    Serial.print("Solar Volt: ");
-    Serial.print(solar_Voltage);
-    Serial.print(" Battery Volt: ");
-    Serial.print(battery_Voltage);
-    Serial.print(" Battery Ratio: ");
-    Serial.print(batt_ratio);
-    Serial.print(" Solar Ratio: ");
-    Serial.println(sol_ratio);
-    // send_udp();
-
-    lastReadingTime = millis();
+  Serial.print("Solar Volt: ");
+  Serial.print(solar_Voltage);
+  Serial.print(" Battery Volt: ");
+  Serial.print(battery_Voltage);
+  //  Serial.print(" Battery Ratio: ");
+  //  Serial.print(batt_ratio);
+  //  Serial.print(" Solar Ratio: ");
+  //  Serial.print(sol_ratio);
+  Serial.print(" Relais: ");
+  if (relais_state){
+    Serial.println("ON");
+  }
+  else{
+    Serial.println("OFF");
   }
 }
+
+
+void send_udp(){
+  // send a reply, to the IP address and port that sent us the packet we received
+  Udp.beginPacket(destination, destPort);
+  Udp.write("solar volt");
+  Udp.write(";");
+  Udp.print(solar_Voltage);
+  Udp.write(";");
+  Udp.write("battery volt");
+  Udp.write(";");
+  Udp.print(battery_Voltage);  
+  Udp.write(";");  
+  Udp.write("relais");
+  Udp.write(";");
+  if (relais_state){
+    Udp.write("ON");
+  }
+  else{
+    Udp.write("OFF");
+  }
+  Udp.endPacket();
+}
+
+void loop() {
+    read_analog();
+    calc_volt();
+//    relais(solar_Voltage);      //relais mit solar voltage aufrufen
+    
+  // check for a reading no more than once a second.
+  if (millis() - lastReadingTime > 1000){
+    send_udp();
+    send_serial();
+    lastReadingTime = millis();
+  }
+
+
+  //alle 15 min = 1000*60*15 in den testmode
+  if ((millis() - lastTestTime > 1000*15) &&! test_mode ){
+    test_mode = true;
+    
+    lastTestStart = millis();
+    lastTestTime = millis();    
+    Serial.println("test START"); 
+  }
+  //10 second later schau ob saft am panel
+  if (millis() - lastTestStart > 1000 && test_mode){
+    //wenn solar größer als batt
+    if ( solar_Voltage > battery_Voltage + 0.5 )
+    {
+      test_mode = false;
+      lastTestTime = millis();
+      Serial.println("test OK");      
+    }
+    else {
+      lastTestStart = millis() ;    
+      Serial.println("test NOK");      
+    }
+
+  }
+
+  relais_state = test_mode;
+  digitalWrite(relais_Pin, relais_state); 
+
+}
+
+
+
+
+
+
 
 
 
