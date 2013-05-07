9a933fd6886b35044308fada86490706724a5cde
hpl <logsoft@gmail.com>
2013-05-08 00:59:48 +0200

    webserver incorporated and tested

--- solarinfo/ardufirm/solarduino/solarduino.ino
+++ solarinfo/ardufirm/solarduino/solarduino.ino
@@ -8,7 +8,7 @@
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 0, 42);
-IPAddress gateway(192,168,0,1);
+IPAddress gateway(192,168,0,1);	
 IPAddress subnet(255, 255, 255, 0);
 IPAddress destination(192, 168, 0, 72);
 unsigned int localPort = 8888;      // local port to listen on
@@ -24,7 +24,7 @@ EthernetUDP Udp;
  * LCD D5 pin to digital pin 4
  * LCD D6 pin to digital pin 3
  * LCD D7 pin to digital pin 2
-*/
+ */
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 int relais_Pin = 9;      // select the pin for the LED
@@ -44,6 +44,8 @@ float sol_ratio = (float)sol_R1 / (float)sol_R2;
 
 float battery_Voltage = 0.0;
 float solar_Voltage = 0.0;
+float battery_leerlauf_Volt = 0.0;
+float solar_leerlauf_Volt = 0.0;
 
 long lastReadingTime = 0;
 long lastTestTime = 0;
@@ -52,19 +54,29 @@ long lastTestStart = 0;
 boolean relais_state = false;
 boolean test_mode = true;
 
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
 
 void setup() {
   // start the Ethernet and UDP:
   Ethernet.begin(mac,ip);
   Udp.begin(localPort);
-  Serial.begin(9600);
-  //inputoutput config
-  pinMode(relais_Pin, OUTPUT);
+  Serial.begin(57600);
+  //inputoutput config 
+  pinMode(relais_Pin, OUTPUT);  
 
-  lcd.begin(16, 2);      // set up the LCD's number of columns and rows:
+  // start the Ethernet connection and the server:
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  lcd.begin(16, 2);      // set up the LCD's number of columns and rows: 
   lcd.print("MOOGAA SOOLAA   V0.2");// Print a message to the LCD.
   Serial.println(" MOOGAA SOOOLAA V0.2 ");
-  Serial.println(" says hello im alive! ");
+  Serial.println(" says hello im alive! ");  
 }
 
 void read_analog (){
@@ -89,11 +101,11 @@ void calc_volt(){
   solar_Voltage = pinVoltage * sol_ratio;    //  Use the ratio calculated for the voltage divider
   /*  A reading of 1 for the A/D = 0.0048mV
    if we multiply the A/D reading by 0.00488 then
-   we get the voltage on the pin.
-
+   we get the voltage on the pin. 
+   
    NOTE! .00488 is ideal. I had to adjust
    to .00610 to match fluke meter.
-
+   
    Also, depending on wiring and
    where voltage is being read, under
    heavy loads voltage displayed can be
@@ -131,8 +143,8 @@ void send_udp(){
   Udp.write(";");
   Udp.write("battery volt");
   Udp.write(";");
-  Udp.print(battery_Voltage);
-  Udp.write(";");
+  Udp.print(battery_Voltage);  
+  Udp.write(";");  
   Udp.write("relais");
   Udp.write(";");
   if (relais_state){
@@ -145,9 +157,9 @@ void send_udp(){
 }
 
 void loop() {
-    read_analog();
-    calc_volt();
-//    relais(solar_Voltage);      //relais mit solar voltage aufrufen
+  read_analog();
+  calc_volt();
+  //    relais(solar_Voltage);      //relais mit solar voltage aufrufen
 
   // check for a reading no more than once a second.
   if (millis() - lastReadingTime > 1000){
@@ -162,29 +174,107 @@ void loop() {
     test_mode = true;
 
     lastTestStart = millis();
-    lastTestTime = millis();
-    Serial.println("test START");
+    lastTestTime = millis();    
+    Serial.println("test START"); 
   }
   //10 second later schau ob saft am panel
   if (millis() - lastTestStart > 1000 && test_mode){
     //wenn solar größer als batt
+    battery_leerlauf_Volt = battery_Voltage;
+    solar_leerlauf_Volt = solar_Voltage;
     if ( solar_Voltage > battery_Voltage + 0.5 )
     {
       test_mode = false;
       lastTestTime = millis();
-      Serial.println("test OK");
+      Serial.println("test OK");      
     }
     else {
-      lastTestStart = millis() ;
-      Serial.println("test NOK");
+      lastTestStart = millis() ;    
+      Serial.println("test NOK");      
     }
 
   }
 
   relais_state = test_mode;
-  digitalWrite(relais_Pin, relais_state);
+  digitalWrite(relais_Pin, relais_state); 
+
+  ///  WEBSERVA ///////
+
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // add a meta refresh tag, so the browser pulls again every 5 seconds:
+          client.println("<meta http-equiv=\"refresh\" content=\"5\">");
+          // output the value of each analog input pin
+
+            client.print("Solar Spannung: ");
+          client.print(solar_Voltage);
+          client.println("<br />");       
+
+          client.print("Batterie Spannung: ");
+          client.print(battery_Voltage);
+          client.println("<br />");       
+
+          client.print("Batterie Leerlauf Spannung: ");
+          client.print(battery_leerlauf_Volt);
+          client.println("<br />");
+
+          client.print("Solar Leerlauf Spannung: ");
+          client.print(solar_leerlauf_Volt);
+          client.println("<br />");
+
+          client.print("Batterie wird : ");
+          if (test_mode){
+            client.print("entladen");
+          }
+          else{
+            client.print("geladen");
+          }
+          client.println("<br />");
+
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+
+
 
 }
+
+
 
 
 
